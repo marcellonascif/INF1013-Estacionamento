b2ecf92e4926c234fc6f7a703142dc4d0553b0f8
Marcello Nascif <marcellobnf@gmail.com>
2024-06-24 18:27:20 -0300

    First Commit
    um teste para ver se o projeto esta funcionando da maneira correta

---
+++ src/model/Vehicle.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Vehicle {
+    private String plate;
+
+    public Vehicle(String plate) {
+        this.plate = plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+}
