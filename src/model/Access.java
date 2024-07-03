bbd3dfe3abc45e5c446e252ad28a087994a23667
Marcello Nascif <marcellobnf@gmail.com>
2024-07-03 12:48:16 -0300

    Added JSON to the project

--- src/model/Access.java
+++ src/model/Access.java
@@ -1,5 +1,11 @@
 package model;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,25 +18,29 @@ public class Access {
         this.vehiclesInside = new ArrayList<Vehicle>();
     }
 
-    public void open(){
+    public void open() {
         System.out.println("abrir");
     }
 
-    public void close(){
+    public void close() {
         System.out.println("fechar");
     }
 
-    public void getRegisteredVehiclesJSON() {
-//        USAR O RESGATE DE JSON AQUI
+    public List<Vehicle> getRegisteredVehiclesJSON() {
+        Gson gson = new Gson();
+        try (FileReader reader = new FileReader("assets/vehicles.json")) {
+            Type vehicleListType = new TypeToken<List<Vehicle>>() {
+            }.getType();
+            return gson.fromJson(reader, vehicleListType);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public void getInsideVehiclesJSON() {
-//        USAR O RESGATE DE JSON AQUI TAMBÉM
-    }
-
-    public Vehicle isVehicleRegistered(String plate){
-        for (Vehicle vehicle : registeredVehicles){
-            if (vehicle.getPlate().equals(plate)){
+    public Vehicle isVehicleRegistered(String plate) {
+        for (Vehicle vehicle : this.registeredVehicles) {
+            if (vehicle.getPlate().equals(plate)) {
                 return vehicle;
             }
         }
@@ -38,9 +48,9 @@ public class Access {
         return null;
     }
 
-    public boolean isVehicleInside(String plate){
-        for (Vehicle vehicle : vehiclesInside){
-            if (vehicle.getPlate().equals(plate)){
+    public boolean isVehicleInside(String plate) {
+        for (Vehicle vehicle : this.vehiclesInside) {
+            if (vehicle.getPlate().equals(plate)) {
                 return true;
             }
         }
@@ -48,29 +58,29 @@ public class Access {
         return false;
     }
 
-    public void registerIn(Vehicle vehicle, ParkingLot parkingLot){
-        vehiclesInside.add(vehicle);
+    public void registerIn(Vehicle vehicle, ParkingLot parkingLot) {
+        this.vehiclesInside.add(vehicle);
         parkingLot.registerIn(vehicle);
     }
 
-    public void registerOut(Vehicle vehicle, ParkingLot parkingLot){
-        vehiclesInside.remove(vehicle);
+    public void registerOut(Vehicle vehicle, ParkingLot parkingLot) {
+        this.vehiclesInside.remove(vehicle);
         parkingLot.registerOut(vehicle);
     }
 
-    public void setRegisteredVehicles(List<Vehicle> registeredVehicles){
+    public void setRegisteredVehicles(List<Vehicle> registeredVehicles) {
         this.registeredVehicles = registeredVehicles;
     }
 
-    public List<Vehicle> getRegisteredVehicles(){
+    public List<Vehicle> getRegisteredVehicles() {
         return this.registeredVehicles;
     }
 
-    public void setVehiclesInside(List<Vehicle> vehiclesInside){
+    public void setVehiclesInside(List<Vehicle> vehiclesInside) {
         this.vehiclesInside = vehiclesInside;
     }
 
-    public List<Vehicle> getVehiclesInside(){
+    public List<Vehicle> getVehiclesInside() {
         return this.vehiclesInside;
     }
 }
--- src/model/AccessControl.java
+++ src/model/AccessControl.java
@@ -1,12 +1,5 @@
 package model;
 
-//import java.io.BufferedReader;
-//import java.io.File;
-//import java.io.FileReader;
-//import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class AccessControl {
 
     public static void accessControlIn(String plate, ParkingLot parkingLot, Access access) {
@@ -37,14 +30,11 @@ public class AccessControl {
     public static void accessControlOut(String plate, ParkingLot parkingLot, Access access) {
         System.out.println("Tentativa de saída para o veículo com placa " + plate);
 
-        // faz sentido essa verificacao? acho que sim pois pode ter acontecido algum problema na entrada do veiculo
-        
         Vehicle vehicle = access.isVehicleRegistered(plate);
         if (vehicle == null) {
             System.out.println("Veículo com placa " + plate + " não está cadastrado\n");
             return;
         }
-
 
         if (!access.isVehicleInside(plate)) {
             System.out.println("Veículo com placa " + plate + " não está dentro do estacionamento\n");
@@ -57,51 +47,23 @@ public class AccessControl {
         System.out.println("Veículo com placa " + plate + " saiu do estacionamento.\n");
     }
 
-//    public static ParkingLot loadJSON(String filename) {
-//        String jsonText = "";
-//        try {
-//            BufferedReader bufferedReader = new BufferedReader(new FileReader(filename));
-//
-//            String line;
-//            while((line = bufferedReader.readLine()) != null){
-//                jsonText += line + "\n";
-//            }
-//
-//            bufferedReader.close();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return new ParkingLot(100); // Valor default para vagas disponíveis
-//        }
-//    }
-
     public static void main(String[] args) {
         ParkingLot parkingCar = new ParkingLot(3);
 
         Access accessCar = new Access();
 
-        Vehicle car1 = new Car("AAA1234");
-        Vehicle car2 = new Car("BBB5678");
-        Vehicle car3 = new Car("CCC2204");
-        Vehicle car4 = new Car("DDD1806");
+        accessCar.setRegisteredVehicles(accessCar.getRegisteredVehiclesJSON());
 
-        List<Vehicle> listCar = new ArrayList<>();
-        listCar.add(car1);
-        listCar.add(car2);
-        listCar.add(car3);
-        listCar.add(car4);
+        // Simulando tentativas de entrada e saída
+        AccessControl.accessControlIn("ABC-1A23", parkingCar, accessCar);
+        AccessControl.accessControlIn("DEF-4B56", parkingCar, accessCar);
+        AccessControl.accessControlIn("CCC-9012", parkingCar, accessCar);
+        AccessControl.accessControlIn("GHI-7C89", parkingCar, accessCar);
+        AccessControl.accessControlIn("JKL-0D12", parkingCar, accessCar);
 
-        accessCar.setRegisteredVehicles(listCar);
-
-        // Simulando tentativas de acesso
-        AccessControl.accessControlIn("AAA1234", parkingCar, accessCar);
-        AccessControl.accessControlIn("BBB5678", parkingCar, accessCar);
-        AccessControl.accessControlIn("CCC9012", parkingCar, accessCar);
-        AccessControl.accessControlIn("CCC2204", parkingCar, accessCar);
-        AccessControl.accessControlIn("DDD1806", parkingCar, accessCar);
-
-        AccessControl.accessControlOut("AAA1234", parkingCar, accessCar);
-        AccessControl.accessControlIn("DDD1806", parkingCar, accessCar);
+        AccessControl.accessControlOut("ABC-1A23", parkingCar, accessCar);
+        AccessControl.accessControlIn("DEF-4B56", parkingCar, accessCar);
+        AccessControl.accessControlOut("YZA-5I67", parkingCar, accessCar);
 
 
     }
